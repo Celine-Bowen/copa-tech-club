b7fc638682cff2fa939e156935777282bc65480a
Kirinyet Brian <52853694+KirinyetBrian@users.noreply.github.com>
2020-06-03 15:57:28 +0300

    edited

--- registration.php
+++ registration.php
@@ -5,28 +5,37 @@
 <html>
 <head>
 	<title>Registration</title>
+	<link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-	<div class="container">
-		<div class="header">		
+	<?php
+include ('includes/nav.php');
+	?>
+	<section id="credentials">
+	<div class="dark">
+				
 			<h2>Register</h2>
 			
 		</div>
 		<form action="registration.php" method="post">
 		<div>
-			<label for="Name">Name</label>
-			<input type="text" name="Name" required>
+			<label for="User Name">Name</label>
+			<input type="text" name="name" required>
 		</div>	
 
 <div>
 			<label for="Registration Number">Registration Number </label>
-			<input type="text" name="Registration Number" required>
+			<input type="text" name="reg" required>
 		</div>	
 
 <div>
-			<label for="Email Address">Email address</label>
-			<input type="email" name="Email Address" required>
+			<label for="CUK Student Email">Email address</label>
+			<input type="text" name="email" required>
 		</div>	
+	<div>
+			<label for="Course">Course</label>
+			<input type="text" name="course" required>
+		</div>
 <div>
 			<label for="Password">Password</label>
 			<input type="Password" name="Password_1" required>
@@ -36,9 +45,14 @@
 			<input type="Password" name="Password_2" required>
 		</div>	
 		<button type="submit" name="register_user"></button>
-		<p>Already a user?<a href="login.php>">log in</a></p>
-		</form>
-	</div>
 
+		<p>
+      Already a member? <a href="login.html">Login</a>
+      <br>
+       </p>
+		</form>
+		
+	</div>
+</section>
 </body>
 </html>
--- server.php
+++ server.php
@@ -1,47 +1,113 @@
 <?php 
-session_start();
 
 
-//initializing variables
-$Name="";
-$Email="";
-$error=array
+// initializing variables
+$password_1="";
+$password_2="";
+$email    = "";
+$errors = array(); 
 
-//connect to db
-$db=mysql_connect("http://localhost/phpmyadmin/tbl_recent_favorite.php?db=credentials&table=user")or die("could not connect to database");
+// connect to the database
+include ('includes/dbconnect.php');
 
-//Register users
+// REGISTER USER
+if (isset($_POST['reg_user']))
+ {
+$name=$_POST['name'];
+$reg=$_POST['reg'];
+$password_1=$_POST['password_1'];
+$password_2=$_POST['password_2'];
+$course=$_POST['course'];
+$email=$_POST['email'];
 
-$Name=mysql_real_escape_string($db, $POST["Name"]);
-$EmailAddress=mysql_real_escape_string($db, $POST["Email Address"]);
-$Password_1=mysql_real_escape_string($db, $POST["Password_1"]);
-$Password_2=mysql_real_escape_string($db, $POST["Password_2"]);
 
-//form validation
-if (empty($Name)) {array_push($error, "Name is required")};
-	if (empty($EmailAddress)) {array_push($error, "Email is required")};
-		if (empty(Password_1)) {array_push($error, "Password_1 is required")};
-		if ($Password_1!=$Password_2)($array_push($error, "Password do not match")};
-			
-	//check db for existing email
-	$user_check_query="SELECT *FROM user WHERE EmailAddress='$EmailAddress'LIMIT 1";
 
-	$results=mysql_query($db, $user_check_query);
-	$user=mysql_fetch_assoc($results?);
+      // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($name)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($password_2)) { array_push($errors, "Confirm password"); }
+  if (empty($course)) { array_push($errors, "course required"); }
+   if (empty($reg)) { array_push($errors, "Reg No required"); }
 
-	if ($user) {
-		if ($user['Email Address']***$EmailAddress){array_push($error,"This email id already exists")};
-		}
 
-		//register user if no errors
-		if ($Password=md5($Password_1); //this will encrypt password
-		$query ="INSERT INTO user (Name,RegistrationNumber, EmailAddress, Password) VALUES ('$NAME',$RegistrationNumber',$EmailAddress',$Password',)";
-		}
-		mysql_query($db, $query);
-		$_SESSION['Name']=$Name;
-		$_SESSION['success']="you are now logged in";
-		header('location;index.html')
+  if ($password_1 != $password_2)
+   {
+  array_push($errors, "The two passwords do not match");
+  }
 
+// first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM user WHERE  RegNo='$reg'  LIMIT 1";
+  $statement=$db->prepare($user_check_query);
+  $statement->execute();
+  $user = $statement->fetchAll();
+  $output='';
+foreach ($user as $row ) 
+{
+      $reg = $row ['RegNo'];
+
+    if ($row['RegNo'] === $reg) 
+    {
+      array_push($errors, "Registration number already exists");
+    }
+
+}
+    if (count($errors) == 0) 
+  {
+    $password_1= md5($password_1);//encrypt the password before saving in the database
+
+// Finally, register user if there are no errors in the form
+  
+  $query = "INSERT INTO user (userName, email, Password,RegNo,Course) 
+          VALUES('$name', '$email', '$password_1','$reg','$course')";
+    $statement=$db->prepare($query);
+    $statement->execute();
+    $results=$statement->fetchAll();    
+      
+    $_SESSION['username'] = $email;
+    $_SESSION['success'] = "You are now logged in";
+    header('location: log.php');
+  
+
+}
+  } 
+  
+
+
+
+//LOG IN 
+
+		if (isset($_POST['login_user'])) {
+  $reg = $_POST['reg'];
+  $password =  $_POST['password'];
+
+  if (empty($reg)) {
+    array_push($errors, "Registration number is required");
+  }
+  if (empty($password)) {
+    array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password);
+    $query = "SELECT * FROM user WHERE RegNo='$reg' AND Password='$password'";
+    $statement=$db->prepare($query);
+    $statement->execute();
+    $results =$statement-> fetchAll();
+    foreach ($results as $row )    
+    {
+      $_SESSION['username'] = $reg;
+      $_SESSION['success'] = "Successfully logged in";
+      header('location: index.php');
+    }
+   
+    {
+      array_push($errors, "Wrong username/password combination");
+    }
+  }
+}
 
 
 
